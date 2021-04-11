997dc8b84bb18558f80099acd8355a15574d5f4e
RasheedAli <you@example.com>
2021-04-11 10:15:52 +0200

    added privacy policy page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {PrivacyComponent} from "./privacy/privacy.component";
+import { HomePageComponent } from './home-page/home-page.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomePageComponent},
+
+  {path: 'privacy', component: PrivacyComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivacyComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
