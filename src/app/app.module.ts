b09d58281d3f5f5ef2e65a42c5eb70d76daf003d
RasheedAli <you@example.com>
2021-06-09 14:32:00 +0200

    made a term of use page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 import {PrivacyComponent} from "./privacy/privacy.component";
 import { HomePageComponent } from './home-page/home-page.component';
+import { TermsComponent } from './terms/terms.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomePageComponent},
 
-  {path: 'privacy', component: PrivacyComponent}
+  {path: 'privacy', component: PrivacyComponent},
+  {path: 'termsofuse', component: TermsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     PrivacyComponent,
-    HomePageComponent
+    HomePageComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
